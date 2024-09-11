633d5ac31b822b36a63deb6e9d0a0bc1e259f671
PoundKake <vance.christian@gmail.com>
2024-09-11 17:48:19 -0400

    cleaning up structure and adding more css transitions

--- src/css-examples/main.js
+++ src/css-examples/main.js
@@ -1,13 +1,24 @@
 import "./style.css";
-import cssLogo from "./CSS3_logo.svg";
+import cssLogo from "/CSS3_logo.svg";
 import { setupCounter } from "./counter.js";
 
 document.querySelector("#app").innerHTML = `
+ <!-- glow -->
   <div>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/CSS" target="_blank">
-      <img src="${cssLogo}" class="logo" alt="CSS logo" />
+    <a href="https://developer.mozilla.org/en-US/docs/Web/CSS">
+      <img src="${cssLogo}" class="glow" alt="CSS logo" />
+    </a>
+  </div>
+   <!-- increase scale -->
+  <div>
+    <a href="https://developer.mozilla.org/en-US/docs/Web/CSS">
+      <img src="${cssLogo}" class="scale-increase" alt="CSS logo" />
+    </a>
+  </div>
+<!-- decrease scale -->
+  <div>
+    <a href="https://developer.mozilla.org/en-US/docs/Web/CSS">
+      <img src="${cssLogo}" class="scale-decrease" alt="CSS logo" />
     </a>
   </div>
 `;
-
-setupCounter(document.querySelector("#counter"));
